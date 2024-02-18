8261678a7f0eb43f473bd899e14d2945b4134f13
mfranmendoza <mfranmendoza@falabella.cl>
2024-02-18 03:55:26 -0300

    feat(main.rs): add struct and bucles

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashSet, vec};
+use std::{collections::{HashMap, HashSet}, vec};
 
 fn main() {
     // hola mundo
@@ -94,5 +94,70 @@ fn main() {
       my_set.insert("Miguel");
       println!("{:?}", my_set);
 
+      // Estructuras : Mapas 
+      // Se debe generar una clave y valor 
+      // estructura muy similar a la de set
+      // estructura no ordenada pero de tipo clave valor
+      // la clave debe ser unica
 
+
+      let mut my_map: HashMap<&str , i32> = vec![("Miguel",38), ("Andres",48), ("Humberto",72)].into_iter().collect();
+      my_map.insert("Francisco", 24);
+      println!("{:?}", my_map);
+
+      // Bucles : Para poder recorrer una estructura 
+      // Bucle de lista
+      for value in my_list.iter() {
+        println!("{}", value);
+      };
+
+      // Bucle de Set
+      for value in my_set.iter() {
+        println!("{}", value);
+      };
+
+      // Bucle con map
+      for (key, value) in my_map.iter() {
+        println!("{} {}",key,  value);
+      };
+
+   
+      // Bucles : While 
+      let mut my_counter = 0;
+      while my_counter < my_list.len()  {
+          println!("{}", my_list[my_counter]);
+          my_counter +=1;
+      }
+      my_function();
+      my_function();
+      my_function();
+
+      // estructuras
+      let my_struct = MyStruct::new("Miguel", 38);
+      println!("{} {}", my_struct.name, my_struct.age)
+}
+
+// este tipo de funcion sirve para poder aislar codigo y utilizarlo 
+// en donde se instancie el nombre de la funcion 
+fn my_function() {
+    println!("Esto es una funcion");
+}
+
+// aunque sea un lenguaje orientado a objetos no tenemos clases 
+// mas bien trabajamos con estructucturas
+
+struct MyStruct {
+  name: String,
+  age: i32
+}
+
+impl MyStruct {
+  // Especifica que `new` retorna una instancia de `MyStruct`
+  fn new(name: &str, age: i32) -> MyStruct {
+      MyStruct {
+          name: name.to_string(),
+          age,
+      }
+      // Nota: No hay punto y coma aquí, lo que permite que Rust retorne esto implícitamente
+  }
 }
